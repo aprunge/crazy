cc323e680e2ca1f9e929ae899abc1d8a072e8917
Sean "Jesus" Cohmer <93715513+aprunge@users.noreply.github.com>
2023-02-14 17:50:25 +0000

    It works now :)

--- fibonacci.pl
+++ fibonacci.pl
@@ -1,1 +1,1 @@
-my ($n) = @ARGV; my ($a, $b) = (0, 1); while ($a <= $n) {($a, $b) = ($b, $a + $b)};}
+my ($n) = @ARGV; my ($a, $b) = (0, 1); for (my $i = 0; $i <= $n; $i++) { ($a, $b) = ($b, $a + $b); print "$a\n"; }
