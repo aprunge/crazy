d9759a0958652cfc99cb2f82a76b3c9f27f16006
Sean "Jesus" Cohmer <93715513+aprunge@users.noreply.github.com>
2023-02-14 17:39:12 +0000

    One-liner

--- fibonacci.pl
+++ fibonacci.pl
@@ -1,10 +1,1 @@
-#!/usr/bin/perl
-my ($n) = @ARGV;
-my $a = 0;
-my $b = 1;
-
-for(my $i = 0; $i <= $n; $i++) {
-  $b = $a + $b;
-  $a = $b - $a;
-  print "$a\n";
-}
+my ($n) = @ARGV; my ($a, $b) = (0, 1); while ($a <= $n) {($a, $b) = ($b, $a + $b)};}
